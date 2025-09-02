d5fffd573ba9ec9436503a86279027fa8706b299
Sathya <neelasathya06@gmail.com>
2025-09-02 07:19:38 -0400

    http package

--- other.go
+++ other.go
@@ -1,11 +1,17 @@
 package main
 
-//type user struct {
-//	name string
-//}
+//import "fmt"
 //
-//type bot interface {
-//	getGreeting(string, int) (string, error)
-//	getBotVersion() float64
-//	respondToUser(user) string
+////type user struct {
+////	name string
+////}
+////
+////type bot interface {
+////	getGreeting(string, int) (string, error)
+////	getBotVersion() float64
+////	respondToUser(user) string
+////}
+//
+//func main1() {
+//	fmt.Println("Hello, Worldss!")
 //}
