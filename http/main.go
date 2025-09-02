412b869ecfaa50a3964b4b9da765a48ff631bb00
Sathya <neelasathya06@gmail.com>
2025-09-02 07:50:31 -0400

    reader and writer package impl

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 
@@ -15,6 +18,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	//bs := make([]byte, 9999)
+	//resp.Body.Read(bs)
+	//fmt.Println(string(bs))
 
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Bytes written: ", len(bs))
+	return len(bs), nil
 }
