854d4432abbe280da238a59d0fa7aa1d658e29bb
Sathya <neelasathya06@gmail.com>
2025-09-02 07:03:39 -0400

    interface impl

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
-package Interfaces
+package main
 
 import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
 
 type englishBot struct{}
 type spanishBot struct{}
@@ -11,11 +15,11 @@ func main() {
 	sb := spanishBot{}
 
 	printGreeting(eb)
-	//printGreeting(sb)
+	printGreeting(sb)
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 //func printGreeting(sb spanishBot) {
